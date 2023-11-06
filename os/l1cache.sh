feb1793c87a9ac1afd6dd8bd47f4adfe366f8adc
Konstantin Shprenger <105585491+bakalover@users.noreply.github.com>
2023-11-06 10:33:40 +0300

    Create l1cache.sh

---
+++ os/l1cache.sh
@@ -0,0 +1,11 @@
+sudo stress-ng --l1cache 4 --l1cache-size 1024 -t 30s --perf --metrics
+sudo stress-ng --l1cache 4 --l1cache-size 2047 -t 30s --perf --metrics
+sudo stress-ng --l1cache 4 --l1cache-size 2048 -t 30s --perf --metrics
+sudo stress-ng --l1cache 4 --l1cache-size 3062 -t 30s --perf --metrics
+sudo stress-ng --l1cache 4 --l1cache-size 4096 -t 30s --perf --metrics
+sudo stress-ng --l1cache 4 --l1cache-size 8191 -t 30s --perf --metrics
+sudo stress-ng --l1cache 4 --l1cache-size 8192 -t 30s --perf --metrics
+sudo stress-ng --l1cache 4 --l1cache-size 12288 -t 30s --perf --metrics
+sudo stress-ng --l1cache 4 --l1cache-size 16383 -t 30s --perf --metrics
+sudo stress-ng --l1cache 4 --l1cache-size 32768 -t 30s --perf --metrics
+sudo stress-ng --l1cache 4 --l1cache-size 65536 -t 30s --perf --metrics
