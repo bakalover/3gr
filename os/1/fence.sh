071a241d1ac85af02f4065155f97605a8c2b8308
Konstantin Shprenger <105585491+bakalover@users.noreply.github.com>
2023-11-06 10:34:55 +0300

    Create fence.sh

---
+++ os/1/fence.sh
@@ -0,0 +1,6 @@
+sudo stress-ng --cache 4 --cache-fence -t 10s --perf --metrics
+sudo stress-ng --cache 4 --cache-fence -t 20s --perf --metrics
+sudo stress-ng --cache 4 --cache-fence -t 30s --perf --metrics
+sudo stress-ng --cache 4 --cache-fence -t 40s --perf --metrics
+sudo stress-ng --cache 4 --cache-fence -t 50s --perf --metrics
+sudo stress-ng --cache 4 --cache-fence -t 60s --perf --metrics
