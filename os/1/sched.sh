fe06824cb2dd6d9f5d0e0c82b8be611e9e9512d6
Konstantin Shprenger <105585491+bakalover@users.noreply.github.com>
2023-11-06 10:30:10 +0300

    Create sched.sh

---
+++ os/1/sched.sh
@@ -0,0 +1,17 @@
+sudo stress-ng --sched deadline --sched-period 1 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 10 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 100 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 1000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 10000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 100000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 200000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 1000000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 20000000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 100000000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 200000000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 1000000000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 20000000000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 100000000000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 200000000000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 1000000000000 --resched 1 --metrics -t 15s
+sudo stress-ng --sched deadline --sched-period 20000000000000 --resched 1 --metrics -t 15s
