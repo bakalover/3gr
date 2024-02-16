7094e052d30c060edce7ae948febabe8bccd15ef
bakalover <kostya@shprenger.com>
2024-02-16 22:00:37 +0300

    Diagrams beta

--- business/demo/src/main/java/business/application/demo/TestConstroller.java
+++ business/demo/src/main/java/business/application/demo/TestConstroller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestConstroller {
     @GetMapping("/")
     public String hello(){
-        return "Haha";
+        return "Hasha";
     }
 }
